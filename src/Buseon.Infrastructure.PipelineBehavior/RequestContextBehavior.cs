de184d9e1f3794c14f8f96a427524cb71bb0b771
rodrigolessa <rodrigolsr@gmail.com>
2026-03-14 17:59:22 -0300

    Move to a specific project

---
+++ src/Buseon.Infrastructure.PipelineBehavior/RequestContextBehavior.cs
@@ -0,0 +1,9 @@
+namespace Buseon.Infrastructure.PipelineBehavior;
+
+/// <summary>
+/// Pipeline é uma extensão arquitetural
+/// </summary>
+public class RequestContextBehavior
+{
+    
+}
---
+++ src/Buseon.Infrastructure.PipelineBehavior/RequestValidationBehavior.cs
@@ -0,0 +1,6 @@
+namespace Buseon.Infrastructure.PipelineBehavior;
+
+public class RequestValidationBehavior
+{
+    
+}
