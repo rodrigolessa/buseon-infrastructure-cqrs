93e4e74b53b363100631055bf9da28462c9afd28
rodrigolessa <rodrigolsr@gmail.com>
2026-03-14 21:19:28 -0300

    Install fluent validation

--- src/Buseon.Infrastructure/Exceptions/FrustratedCommandExecutionException.cs
+++ src/Buseon.Infrastructure/Exceptions/FrustratedCommandExecutionException.cs
@@ -1,4 +1,4 @@
-//using FluentValidation.Results;
+using FluentValidation.Results;
 
 namespace Buseon.Infrastructure.Exceptions;
 
@@ -7,8 +7,7 @@ public class FrustratedCommandExecutionException : Exception
     private const string FrustratedCommandExecutionMessage = "Frustrated command execution";
     
     // TODO: Create a function to concatenate errors list into string 
-    //public FrustratedCommandExecutionException(IEnumerable<ValidationFailure> errors) : base(FrustratedCommandExecutionMessage)
-    public FrustratedCommandExecutionException(IEnumerable<string> errors) : base(FrustratedCommandExecutionMessage)
+    public FrustratedCommandExecutionException(IEnumerable<ValidationFailure> errors) : base(FrustratedCommandExecutionMessage)
     {
     }
 }
--- src/Buseon.Infrastructure/Exceptions/FrustratedQueryExecutionException.cs
+++ src/Buseon.Infrastructure/Exceptions/FrustratedQueryExecutionException.cs
@@ -1,30 +1,26 @@
-//using FluentValidation;
-//using FluentValidation.Results;
+using FluentValidation;
+using FluentValidation.Results;
 
 namespace Buseon.Infrastructure.Exceptions;
 
 [Serializable]
-public class FrustratedQueryExecutionException //: ValidationException
+public class FrustratedQueryExecutionException : ValidationException
 {
     private const string FrustratedQueryExecutionMessage = "Frustrated query execution";
 
-    //public FrustratedQueryExecutionException(string message) : base(message)
-    public FrustratedQueryExecutionException(string message)
+    public FrustratedQueryExecutionException(string message) : base(message)
     {
     }
 
-    //public FrustratedQueryExecutionException(string message, IEnumerable<ValidationFailure> errors) : base(message, errors)
-    public FrustratedQueryExecutionException(string message, IEnumerable<string> errors)
+    public FrustratedQueryExecutionException(string message, IEnumerable<ValidationFailure> errors) : base(message, errors)
     {
     }
 
-    //public FrustratedQueryExecutionException(string message, IEnumerable<ValidationFailure> errors, bool appendDefaultMessage) : base(message, errors, appendDefaultMessage)
-    public FrustratedQueryExecutionException(string message, IEnumerable<string> errors, bool appendDefaultMessage)
+    public FrustratedQueryExecutionException(string message, IEnumerable<ValidationFailure> errors, bool appendDefaultMessage) : base(message, errors, appendDefaultMessage)
     {
     }
 
-    //public FrustratedQueryExecutionException(IEnumerable<ValidationFailure> errors) : base(FrustratedQueryExecutionMessage, errors)
-    public FrustratedQueryExecutionException(IEnumerable<string> errors)
+    public FrustratedQueryExecutionException(IEnumerable<ValidationFailure> errors) : base(FrustratedQueryExecutionMessage, errors)
     {
     }
 }
